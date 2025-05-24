522ed13fb44d1e499e0343077da15396012a5306
samlocal <sameerndl28@gmail.com>
2025-05-24 14:39:56 +0530

    fc

---
+++ src/main/java/com/example1/demoz/DemozApplication.java
@@ -0,0 +1,14 @@
+package com.example1.demoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemozApplication {
+
+	public static void main(String[] args) {
+		int x=99;
+		SpringApplication.run(DemozApplication.class, args);
+	}
+
+}
